ee3961391f54af82c83bde523876002bf9730e81
Anderson Araujo <andersonigt85@gmail.com>
2023-08-18 19:52:36 -0300

    Atualizacao dia 18-08

--- Aulas_GO/09/arrays.go
+++ Aulas_GO/09/arrays.go
@@ -1,4 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var a [5]int
+	fmt.Println("Array: ", a)
+
+	a[4] = 100
+	fmt.Println("Depois de receber 100 na posição 4:", a)
+	fmt.Println("O elemento na 4ª posição: ", a[4])
+	fmt.Println("O tamanho do array: ", len(a))
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("Array b: ", b)
+	var twoD [2][3]int
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 3; j++ {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println("Array 2d: ", twoD)
 }
